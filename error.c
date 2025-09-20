9462b4336d1bf66f131fce057cf919ea307a56af
Harshi <srisaiharshitagollapalli@gmail.com>
2025-09-20 18:02:14 +0530

    Error fixed

--- error.c
+++ error.c
@@ -10,7 +10,7 @@ void merge(int arr[], int l, int m, int r) {
     for(int i=0;i<n1;i++)
         L[i] = arr[l + i];
     for(int j=0;j<n2;j++)
-        R[j] = arr[j]; 
+        R[j] = arr[m+1+j]; 
 
     int i=0,j=0,k=l;
     while(i<n1 && j<n2) {
